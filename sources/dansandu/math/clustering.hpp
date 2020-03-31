6ad74c46172a159eee1c57dee6c4342a1dfd8516
dansandu <3251086-dansandu@users.noreply.gitlab.com>
2020-03-31 15:43:09 +0300

    Add k-means clustering

--- sources/dansandu/math/clustering.hpp
+++ sources/dansandu/math/clustering.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "dansandu/ballotin/default_random_generator.hpp"
 #include "dansandu/ballotin/exception.hpp"
 #include "dansandu/math/common.hpp"
 #include "dansandu/math/matrix.hpp"
@@ -8,20 +9,16 @@
 namespace dansandu::math::clustering
 {
 
-template<typename T, int N>
+template<typename T, int N,
+         typename RandomGenerator = dansandu::ballotin::default_random_generator::DefaultRandomGenerator>
 std::pair<std::vector<dansandu::math::matrix::Matrix<T, 1, N>>, std::vector<int>>
     kMeans(std::vector<dansandu::math::matrix::Matrix<T, 1, N>> samples, int clusterCount, int maximumIterations)
 {
-    using dansandu::math::common::multiplicative_identity;
-    using dansandu::math::matrix::distance;
-    using namespace dansandu::range::range;
-
-    static std::random_device randomDevice;
-    static std::mt19937 generator{randomDevice()};
     if (clusterCount <= 0)
         THROW(std::invalid_argument, "invalid cluster count ", clusterCount, " -- cluster count must be positive");
 
-    auto centroids = integers(0, 1, samples.size()) | shuffle() | take(clusterCount) |
+    using namespace dansandu::range::range;
+    auto centroids = integers(0, 1, samples.size()) | shuffle<RandomGenerator>() | take(clusterCount) |
                      map([&samples](auto i) { return samples[i]; }) | toVector();
     auto labels = std::vector<int>(samples.size());
     auto newCentroids = centroids;
@@ -33,10 +30,10 @@ std::pair<std::vector<dansandu::math::matrix::Matrix<T, 1, N>>, std::vector<int>
         for (auto sample = 0U; sample < samples.size(); ++sample)
         {
             auto bestCentroid = 0;
-            auto bestDistance = -multiplicative_identity<T>;
+            auto bestDistance = -dansandu::math::common::multiplicative_identity<T>;
             for (auto centroid = 0U; centroid < centroids.size(); ++centroid)
             {
-                auto candidateDistance = distance(centroids[centroid], samples[sample]);
+                auto candidateDistance = dansandu::math::matrix::distance(centroids[centroid], samples[sample]);
                 if (bestDistance < 0 || candidateDistance < bestDistance)
                 {
                     bestDistance = candidateDistance;
--- sources/dansandu/math/clustering.test.cpp
+++ sources/dansandu/math/clustering.test.cpp
@@ -1,7 +1,11 @@
 #include "catchorg/catch/catch.hpp"
+#include "dansandu/ballotin/predictive_random_generator.hpp"
 #include "dansandu/math/clustering.hpp"
+#include "dansandu/math/matrix.hpp"
 
+using dansandu::ballotin::predictive_random_generator::PredictiveRandomGenerator;
 using dansandu::math::clustering::kMeans;
+using dansandu::math::matrix::close;
 using Point = dansandu::math::matrix::Matrix<double, 1, 2>;
 
 TEST_CASE("Clustering")
@@ -19,13 +23,20 @@ TEST_CASE("Clustering")
             std::vector<Point>{{Point{{-3.0, 6.0}}, Point{{-7.0, 5.0}}, Point{{-8.0, 4.0}}, Point{{-7.0, 3.0}},
                                 Point{{-4.0, 2.0}}, Point{{5.0, 1.0}}, Point{{8.0, 0.0}}, Point{{7.0, -1.0}},
                                 Point{{9.0, -2.0}}, Point{{5.0, -4.0}}, Point{{6.0, -4.5}}}};
-
-        auto maximumIterations = 15;
         auto clusterCount = 2;
-        auto [centroids, labels] = kMeans(samples, clusterCount, maximumIterations);
+        auto maximumIterations = 10;
+        auto precision = 1.0e-4;
+        auto expectedCentroids = std::make_pair(Point{{6.66667, -1.75}}, Point{{-5.8, 4.0}});
+        auto expectedLabels = std::vector<int>{{1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0}};
+        auto [centroids, labels] =
+            kMeans<double, 2, PredictiveRandomGenerator>(samples, clusterCount, maximumIterations);
 
         REQUIRE(centroids.size() == clusterCount);
 
-        REQUIRE(labels.size() == samples.size());
+        REQUIRE(close(centroids[0], expectedCentroids.first, precision));
+
+        REQUIRE(close(centroids[1], expectedCentroids.second, precision));
+
+        REQUIRE(labels == expectedLabels);
     }
 }
