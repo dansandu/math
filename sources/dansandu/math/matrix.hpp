b3a8daf9595262a53f7da8ba69a8297a83fc72fa
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2020-10-24 23:49:49 +0300

    Dynamic matrix WIP #2

--- sources/dansandu/math/matrix.hpp
+++ sources/dansandu/math/matrix.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "dansandu/ballotin/exception.hpp"
 #include "dansandu/math/common.hpp"
 
 #include <array>
@@ -24,11 +25,12 @@ struct DataStorageStrategyFor
     constexpr static auto stackBytes = static_cast<size_type>(32);
 
     constexpr static auto value = (M == dynamic || N == dynamic || M * N * sizeof(Type) > stackBytes)
-            ? DataStorageStrategy::heap
-            : DataStorageStrategy::stack;
+                                      ? DataStorageStrategy::heap
+                                      : DataStorageStrategy::stack;
 };
 
-template<typename Type, size_type M, size_type N, DataStorageStrategy dataStorageStrategy>
+template<typename Type, size_type M, size_type N,
+         DataStorageStrategy dataStorageStrategy = DataStorageStrategyFor<Type, M, N>::value>
 struct DataStorage;
 
 template<typename Type, size_type M, size_type N>
@@ -53,7 +55,13 @@ struct DimensionalityStorage;
 template<typename Type>
 struct DimensionalityStorage<Type, dynamic, dynamic>
 {
-    constexpr DimensionalityStorage() noexcept : rows{0}, columns{0} { }
+    constexpr DimensionalityStorage() noexcept : rows{0}, columns{0}
+    {
+    }
+
+    constexpr DimensionalityStorage(size_type r, size_type c) noexcept : rows{r}, columns{c}
+    {
+    }
 
     size_type rows;
     size_type columns;
@@ -62,7 +70,13 @@ struct DimensionalityStorage<Type, dynamic, dynamic>
 template<typename Type, size_type N>
 struct DimensionalityStorage<Type, dynamic, N>
 {
-    constexpr DimensionalityStorage() noexcept : rows{0} { }
+    constexpr DimensionalityStorage() noexcept : rows{0}
+    {
+    }
+
+    constexpr DimensionalityStorage(size_type r, size_type) noexcept : rows{r}
+    {
+    }
 
     size_type rows;
 };
@@ -70,7 +84,13 @@ struct DimensionalityStorage<Type, dynamic, N>
 template<typename Type, size_type M>
 struct DimensionalityStorage<Type, M, dynamic>
 {
-    constexpr DimensionalityStorage() noexcept : columns{0} { }
+    constexpr DimensionalityStorage() noexcept : columns{0}
+    {
+    }
+
+    constexpr DimensionalityStorage(size_type, size_type c) noexcept : columns{c}
+    {
+    }
 
     size_type columns;
 };
@@ -78,80 +98,140 @@ struct DimensionalityStorage<Type, M, dynamic>
 template<typename Type, size_type M, size_type N>
 struct DimensionalityStorage
 {
+    constexpr DimensionalityStorage() noexcept
+    {
+    }
 
+    constexpr DimensionalityStorage(size_type, size_type) noexcept
+    {
+    }
 };
 
-template<typename Type, size_type M, size_type N>
+template<typename Type = double, size_type M = dynamic, size_type N = dynamic>
 class Matrix : private DimensionalityStorage<Type, M, N>, private DataStorage<Type, M, N>
 {
+    constexpr static auto isVectorOfMinimumLength(size_type length) noexcept
+    {
+        return ((M == 1 || M == dynamic) && (N == dynamic || N >= length)) ||
+               ((M == dynamic || M >= length) && (N == 1 || N == dynamic));
+    }
+
+    constexpr auto canSubscript(size_type index) const noexcept
+    {
+        return ((rowCount() == 1) & (index < columnCount())) | ((columnCount() == 1) & (index < rowCount()));
+    }
+
+    constexpr auto canSubscript(size_type row, size_type column) const noexcept
+    {
+        return (row >= 0) & (row < rowCount()) & (column >= 0) & (column < columnCount());
+    }
+
+    constexpr auto getIndex(size_type index) const noexcept
+    {
+        return index;
+    }
+
+    constexpr auto getIndex(size_type row, size_type column) const noexcept
+    {
+        return row * columnCount() + column;
+    }
+
 public:
     static_assert(M >= 0 || M == dynamic, "matrix static rows must be positive or dynamic");
     static_assert(N >= 0 || N == dynamic, "matrix static columns must be positive or dynamic");
     static_assert((M == 0) == (N == 0), "zero static rows imply zero static columns and vice-versa");
-    
+
     using value_type = Type;
 
-    Matrix() noexcept(strategy == DataStorageStrategy::stack)
+    constexpr Matrix() noexcept(DataStorage<Type, M, N>::strategy == DataStorageStrategy::stack)
     {
-        if constexpr (strategy == DataStorageStrategy::stack)
+        if constexpr (DataStorage<Type, M, N>::strategy == DataStorageStrategy::stack)
         {
-            std::fill(data.begin(), data.end(), additive_identity<value_type>);
+            std::fill(DataStorage<Type, M, N>::data.begin(), DataStorage<Type, M, N>::data.end(),
+                      dansandu::math::common::additive_identity<value_type>);
         }
         else if constexpr (M != dynamic && N != dynamic)
         {
-            data = std::vector<value_type>(M * N, additive_identity<value_type>);
+            DataStorage<Type, M, N>::data =
+                std::vector<value_type>(M * N, dansandu::math::common::additive_identity<value_type>);
         }
     }
 
-    template<typename U = Type, size_type L,
-             typename = std::enable_if_t<(M == L && N == 1) ||
-                                         (M == 1 && N == L) ||
-                                         (M == dynamic && N == 1) ||
-                                         (M == 1       && N == dynamic) ||
-                                         (M == dynamic && N == L) ||
-                                         (M == L       && N == dynamic) ||
-                                         (M == dynamic && N == dynamic), U>>
-    explicit Matrix(const value_type (&array)[L]) noexcept(strategy == DataStorageStrategy::stack)
+    template<size_type len, typename T = Type,
+             typename = std::enable_if_t<(M == len && N == 1) || (M == 1 && N == len) || (M == dynamic && N == 1) ||
+                                             (M == 1 && N == dynamic) || (M == dynamic && N == len) ||
+                                             (M == len && N == dynamic) || (M == dynamic && N == dynamic),
+                                         T>>
+    explicit constexpr Matrix(const value_type (&array)[len]) noexcept(DataStorage<Type, M, N>::strategy ==
+                                                                       DataStorageStrategy::stack)
     {
         if constexpr (M == dynamic)
         {
-            if constexpr (N == L)
+            if constexpr (N == len)
             {
-                rows = 1;
+                DimensionalityStorage<Type, M, N>::rows = 1;
             }
             else
             {
-                rows = L;
-            } 
+                DimensionalityStorage<Type, M, N>::rows = len;
+            }
         }
 
         if constexpr (N == dynamic)
         {
             if constexpr (M == 1)
             {
-                columns = L;
+                DimensionalityStorage<Type, M, N>::columns = len;
             }
             else
             {
-                columns = 1;
+                DimensionalityStorage<Type, M, N>::columns = 1;
             }
         }
 
-        if constexpr (strategy == DataStorageStrategy::stack)
+        if constexpr (DataStorage<Type, M, N>::strategy == DataStorageStrategy::stack)
         {
-            std::copy(array, array + L, data.begin());
+            std::copy(array, array + len, DataStorage<Type, M, N>::data.begin());
         }
         else
         {
-            data = std::vector<value_type>{array, array + L};
+            DataStorage<Type, M, N>::data = std::vector<value_type>{array, array + len};
         }
     }
 
-    auto rowCount() const noexcept
+    template<size_type m, size_type n, typename T = Type,
+             typename = std::enable_if_t<(M == m && N == n) || (M == m && N == dynamic) || (M == dynamic && N == n) ||
+                                             (M == dynamic && N == dynamic),
+                                         T>>
+    explicit constexpr Matrix(const value_type (&array)[m][n]) noexcept(DataStorage<Type, M, N>::strategy ==
+                                                                        DataStorageStrategy::stack)
+        : DimensionalityStorage<Type, M, N>{m, n}
+    {
+        if constexpr (DataStorage<Type, M, N>::strategy == DataStorageStrategy::heap)
+        {
+            DataStorage<Type, M, N>::data.reserve(m * n);
+        }
+        for (auto row = 0; row < m; ++row)
+        {
+            for (auto column = 0; column < n; ++column)
+            {
+                if constexpr (DataStorage<Type, M, N>::strategy == DataStorageStrategy::stack)
+                {
+                    DataStorage<Type, M, N>::data[getIndex(row, column)] = array[row][column];
+                }
+                else
+                {
+                    DataStorage<Type, M, N>::data.push_back(array[row][column]);
+                }
+            }
+        }
+    }
+
+    constexpr auto rowCount() const noexcept
     {
         if constexpr (M == dynamic)
         {
-            return rows;
+            return DimensionalityStorage<Type, M, N>::rows;
         }
         else
         {
@@ -159,11 +239,11 @@ public:
         }
     }
 
-    auto columnCount() const noexcept
+    constexpr auto columnCount() const noexcept
     {
         if constexpr (N == dynamic)
         {
-            return columns;
+            return DimensionalityStorage<Type, M, N>::columns;
         }
         else
         {
@@ -174,58 +254,208 @@ public:
     template<typename T = Type, typename = std::enable_if_t<M == dynamic || M == 1 || N == dynamic || N == 1, T>>
     auto length() const
     {
-        if constexpr (M == dynamic)
+        if (rowCount() == 1 || columnCount() == 1)
         {
-            if constexpr (N == dynamic)
-            {
-                if (rows == 1 || columns == 1)
-                {
-                    return rows * columns;
-                }
-                else
-                {
-                    THROW(std::runtime_error, "Matrix is not a vector");
-                }
-            }
-            else if constexpr (N == 1)
-            {
-                return rows;
-            }
-            else 
-            {
-                if (rows == 1)
-                {
-                    return N;
-                }
-                else
-                {
-                    THROW(std::runtime_error, "Matrix is not a vector");
-                }
-            }
-        }
-        else if constexpr (N == dynamic)
-        {
-            if constexpr (M == 1)
-            {
-                return rows;
-            }
-            else
-            {
-                if (columns == 1)
-                {
-                    return M;
-                }
-                else
-                {
-                    THROW(std::runtime_error, "Matrix is not a vector");
-                }
-            }
+            return rowCount() * columnCount();
         }
         else
         {
-            return M * N;
+            THROW(std::runtime_error, "cannot get the length of non-vector matrix");
         }
     }
+
+    template<typename T = Type, typename = std::enable_if_t<M != 0 && N != 0, T>>
+    auto& operator()(size_type row, size_type column)
+    {
+        if (canSubscript(row, column))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(row, column)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot index the (", row, ", ", column, ") element in a ", rowCount(), "x",
+                  columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<M != 0 && N != 0, T>>
+    const auto& operator()(size_type row, size_type column) const
+    {
+        if (canSubscript(row, column))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(row, column)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot index the (", row, ", ", column, ") element in a ", rowCount(), "x",
+                  columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<M == dynamic || N == dynamic || M == 1 || N == 1, T>>
+    auto& operator()(size_type coordinate)
+    {
+        if (canSubscript(coordinate))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(coordinate)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot index the ", coordinate, "th element of a ", rowCount(), "x",
+                  columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<M == dynamic || N == dynamic || M == 1 || N == 1, T>>
+    const auto& operator()(size_type coordinate) const
+    {
+        if (canSubscript(coordinate))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(coordinate)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot index the ", coordinate, "th element of a ", rowCount(), "x",
+                  columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<isVectorOfMinimumLength(1), T>>
+    auto& x()
+    {
+        if (canSubscript(0))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(0)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot get the x coordinate of a ", rowCount(), "x", columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<isVectorOfMinimumLength(1), T>>
+    const auto& x() const
+    {
+        if (canSubscript(0))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(0)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot get the x coordinate of a ", rowCount(), "x", columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<isVectorOfMinimumLength(2), T>>
+    auto& y()
+    {
+        if (canSubscript(1))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(1)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot get the y coordinate of a ", rowCount(), "x", columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<isVectorOfMinimumLength(2), T>>
+    const auto& y() const
+    {
+        if (canSubscript(1))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(1)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot get the y coordinate of a ", rowCount(), "x", columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<isVectorOfMinimumLength(3), T>>
+    auto& z()
+    {
+        if (canSubscript(2))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(2)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot get the z coordinate of a ", rowCount(), "x", columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<isVectorOfMinimumLength(3), T>>
+    const auto& z() const
+    {
+        if (canSubscript(2))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(2)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot get the z coordinate of a ", rowCount(), "x", columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<isVectorOfMinimumLength(4), T>>
+    auto& w()
+    {
+        if (canSubscript(3))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(3)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot get the w coordinate of a ", rowCount(), "x", columnCount(), " matrix");
+        }
+    }
+
+    template<typename T = Type, typename = std::enable_if_t<isVectorOfMinimumLength(4), T>>
+    const auto& w() const
+    {
+        if (canSubscript(3))
+        {
+            return DataStorage<Type, M, N>::data[getIndex(3)];
+        }
+        else
+        {
+            THROW(std::out_of_range, "cannot get the w coordinate of a ", rowCount(), "x", columnCount(), " matrix");
+        }
+    }
+
+    auto begin() const
+    {
+        return DataStorage<Type, M, N>::data.cbegin();
+    }
+
+    auto end() const
+    {
+        return DataStorage<Type, M, N>::data.cend();
+    }
+
+    auto cbegin() const
+    {
+        return DataStorage<Type, M, N>::data.cbegin();
+    }
+
+    auto cend() const
+    {
+        return DataStorage<Type, M, N>::data.cend();
+    }
 };
+
+template<typename Type, size_type M, size_type N, typename = std::enable_if_t<std::is_integral_v<Type>>>
+auto operator==(const Matrix<Type, M, N>& a, const Matrix<Type, M, N>& b)
+{
+    return ((a.rowCount() == b.rowCount()) & (a.columnCount() == b.columnCount())) &&
+           std::equal(a.cbegin(), a.cend(), b.cbegin());
+}
+
+template<typename Type, size_type M, size_type N, typename = std::enable_if_t<std::is_integral_v<Type>>>
+auto operator!=(const Matrix<Type, M, N>& a, const Matrix<Type, M, N>& b)
+{
+    return !(a == b);
+}
 
 }
--- sources/dansandu/math/matrix.test.cpp
+++ sources/dansandu/math/matrix.test.cpp
@@ -4,204 +4,210 @@
 #include <stdexcept>
 
 using Catch::Detail::Approx;
-using dansandu::math::matrix::close;
-using dansandu::math::matrix::crossProduct;
-using dansandu::math::matrix::distance;
-using dansandu::math::matrix::dotProduct;
-using dansandu::math::matrix::magnitude;
+using dansandu::math::matrix::dynamic;
 using dansandu::math::matrix::Matrix;
-using dansandu::math::matrix::normalized;
 
 TEST_CASE("dansandu::math::matrix::Matrix")
 {
     SECTION("default constructor")
     {
-        auto vector = Matrix<int, 3, 1>{};
+        SECTION("static x static matrix")
+        {
+            SECTION("null matrix")
+            {
+                auto matrix = Matrix<int, 0, 0>{};
 
-        REQUIRE(vector.rowCount() == 3);
+                REQUIRE(matrix.rowCount() == 0);
 
-        REQUIRE(vector.columnCount() == 1);
+                REQUIRE(matrix.columnCount() == 0);
+            }
 
-        REQUIRE(vector.length() == 3);
+            SECTION("row vector")
+            {
+                auto vector = Matrix<int, 1, 2>{};
 
-        REQUIRE(vector(0, 0) == 0);
+                REQUIRE(vector.rowCount() == 1);
 
-        REQUIRE(vector(1, 0) == 0);
+                REQUIRE(vector.columnCount() == 2);
 
-        REQUIRE(vector(2, 0) == 0);
+                REQUIRE(vector.length() == 2);
+
+                REQUIRE(vector(0, 0) == 0);
+
+                REQUIRE(vector(0, 1) == 0);
+
+                REQUIRE_THROWS_AS(vector(2), std::out_of_range);
+
+                REQUIRE_THROWS_AS(vector(0, 2), std::out_of_range);
+
+                REQUIRE_THROWS_AS(vector(1, 0), std::out_of_range);
+            }
+
+            SECTION("column vector")
+            {
+                auto vector = Matrix<int, 3, 1>{};
+
+                REQUIRE(vector.rowCount() == 3);
+
+                REQUIRE(vector.columnCount() == 1);
+
+                REQUIRE(vector.length() == 3);
+
+                REQUIRE(vector(0, 0) == 0);
+
+                REQUIRE(vector(1, 0) == 0);
+
+                REQUIRE(vector(2, 0) == 0);
+
+                REQUIRE_THROWS_AS(vector(3), std::out_of_range);
+
+                REQUIRE_THROWS_AS(vector(0, 1), std::out_of_range);
+
+                REQUIRE_THROWS_AS(vector(3, 0), std::out_of_range);
+            }
+
+            SECTION("non-vector")
+            {
+                auto matrix = Matrix<int, 2, 3>{};
+
+                REQUIRE(matrix.rowCount() == 2);
+
+                REQUIRE(matrix.columnCount() == 3);
+
+                REQUIRE(matrix(0, 0) == 0);
+
+                REQUIRE(matrix(0, 1) == 0);
+
+                REQUIRE(matrix(0, 2) == 0);
+
+                REQUIRE(matrix(1, 0) == 0);
+
+                REQUIRE(matrix(1, 1) == 0);
+
+                REQUIRE(matrix(1, 2) == 0);
+
+                REQUIRE_THROWS_AS(matrix(0, 3), std::out_of_range);
+
+                REQUIRE_THROWS_AS(matrix(3, 0), std::out_of_range);
+            }
+        }
+
+        SECTION("null dynamic x dynamic matrix")
+        {
+            auto matrix = Matrix<int, dynamic, dynamic>{};
+
+            REQUIRE(matrix.rowCount() == 0);
+
+            REQUIRE(matrix.columnCount() == 0);
+        }
+
+        SECTION("null static x dynamic matrix")
+        {
+            auto matrix = Matrix<int, 2, dynamic>{};
+
+            REQUIRE(matrix.rowCount() == 2);
+
+            REQUIRE(matrix.columnCount() == 0);
+
+            REQUIRE_THROWS_AS(matrix.length(), std::runtime_error);
+
+            REQUIRE_THROWS_AS(matrix(0), std::out_of_range);
+
+            REQUIRE_THROWS_AS(matrix(0, 0), std::out_of_range);
+
+            REQUIRE_THROWS_AS(matrix.x(), std::out_of_range);
+        }
+
+        SECTION("null dynamic x static matrix")
+        {
+            auto matrix = Matrix<int, dynamic, 1>{};
+
+            REQUIRE(matrix.rowCount() == 0);
+
+            REQUIRE(matrix.columnCount() == 1);
+
+            REQUIRE(matrix.length() == 0);
+
+            REQUIRE_THROWS_AS(matrix(0), std::out_of_range);
+
+            REQUIRE_THROWS_AS(matrix(0, 0), std::out_of_range);
+
+            REQUIRE_THROWS_AS(matrix.x(), std::out_of_range);
+        }
     }
 
-    SECTION("1D array constructor")
+    SECTION("vector initialization")
     {
-        auto vector = Matrix<int, 1, 3>{7, 11, 13};
+        SECTION("static vector")
+        {
+            auto vector = Matrix<int, 4, 1>{{11, 13, 17, 23}};
 
-        REQUIRE(vector.rowCount() == 1);
+            REQUIRE(vector.rowCount() == 4);
 
-        REQUIRE(vector.columnCount() == 3);
+            REQUIRE(vector.columnCount() == 1);
 
-        REQUIRE(vector.length() == 3);
+            REQUIRE(vector.length() == 4);
 
-        REQUIRE(vector(0, 0) == 7);
+            REQUIRE(vector(0) == 11);
 
-        REQUIRE(vector(0, 1) == 11);
+            REQUIRE(vector(1) == 13);
 
-        REQUIRE(vector(0, 2) == 13);
+            REQUIRE(vector(2) == 17);
+
+            REQUIRE(vector(3) == 23);
+
+            REQUIRE(vector.x() == 11);
+
+            REQUIRE(vector.y() == 13);
+
+            REQUIRE(vector.z() == 17);
+
+            REQUIRE(vector.w() == 23);
+
+            REQUIRE(vector(0, 0) == 11);
+
+            REQUIRE(vector(1, 0) == 13);
+
+            REQUIRE(vector(2, 0) == 17);
+
+            REQUIRE(vector(3, 0) == 23);
+
+            auto sameVector = Matrix<int, 4, 1>{{11, 13, 17, 23}};
+
+            REQUIRE(vector == sameVector);
+
+            auto otherVector = Matrix<int, 4, 1>{{11, 13, 17, 20}};
+
+            REQUIRE(vector != otherVector);
+        }
     }
 
-    SECTION("2D array constructor")
+    SECTION("matrix initialization")
     {
-        auto matrix = Matrix<int, 2, 3>{{{17, 19, 23}, {29, 31, 37}}};
+        auto matrix = Matrix<int, 2, 3>{{{1, 2, 3}, {11, 12, 13}}};
 
         REQUIRE(matrix.rowCount() == 2);
 
         REQUIRE(matrix.columnCount() == 3);
 
-        REQUIRE(matrix(0, 0) == 17);
+        REQUIRE(matrix(0, 0) == 1);
 
-        REQUIRE(matrix(0, 1) == 19);
+        REQUIRE(matrix(0, 1) == 2);
 
-        REQUIRE(matrix(0, 2) == 23);
+        REQUIRE(matrix(0, 2) == 3);
 
-        REQUIRE(matrix(1, 0) == 29);
+        REQUIRE(matrix(1, 0) == 11);
 
-        REQUIRE(matrix(1, 1) == 31);
+        REQUIRE(matrix(1, 1) == 12);
 
-        REQUIRE(matrix(1, 2) == 37);
-    }
+        REQUIRE(matrix(1, 2) == 13);
 
-    SECTION("fill constructor")
-    {
-        auto matrix = Matrix<int, 3, 2>{-7};
-        auto expected = {-7, -7, -7, -7, -7, -7};
+        auto sameMatrix = Matrix<int, 2, 3>{{{1, 2, 3}, {11, 12, 13}}};
 
-        REQUIRE(std::equal(matrix.cbegin(), matrix.cend(), expected.begin(), expected.end()));
-    }
+        REQUIRE(matrix == sameMatrix);
 
-    SECTION("components constructor")
-    {
-        auto x = 3;
-        auto y = 5;
-        auto z = 7;
-        auto matrix = Matrix<int, 3, 1>{x, y, z};
+        auto otherMatrix = Matrix<int, 2, 3>{{{1, 2, 3}, {11, 22, 13}}};
 
-        REQUIRE(matrix.x() == x);
-
-        REQUIRE(matrix.y() == y);
-
-        REQUIRE(matrix.z() == z);
-    }
-
-    SECTION("bad indexing throws")
-    {
-        auto vector = Matrix<int, 3, 1>{};
-
-        REQUIRE_THROWS_AS(vector(3, 1), std::out_of_range);
-
-        REQUIRE_THROWS_AS(vector(-1, 0), std::out_of_range);
-
-        REQUIRE_THROWS_AS(vector(0, -1), std::out_of_range);
-
-        REQUIRE_THROWS_AS(vector(2, 2), std::out_of_range);
-
-        REQUIRE_THROWS_AS(vector(4, 0), std::out_of_range);
-    }
-
-    SECTION("vector subscript")
-    {
-        auto vector = Matrix<int, 3, 1>{1, 3, 5};
-
-        REQUIRE(vector.x() == 1);
-
-        REQUIRE(vector.y() == 3);
-
-        REQUIRE(vector.z() == 5);
-
-        REQUIRE(vector(0) == 1);
-
-        REQUIRE(vector(1) == 3);
-
-        REQUIRE(vector(2) == 5);
-    }
-
-    SECTION("binary matrix-matrix operations")
-    {
-        auto a = Matrix<int, 3, 1>{10, 15, 20};
-        auto b = Matrix<int, 3, 1>{1, 3, 5};
-
-        SECTION("addition")
-        {
-            REQUIRE(a + b == Matrix<int, 3, 1>{11, 18, 25});
-        }
-
-        SECTION("subtraction")
-        {
-            REQUIRE(a - b == Matrix<int, 3, 1>{9, 12, 15});
-        }
-
-        SECTION("dot product")
-        {
-            REQUIRE(dotProduct(a, b) == 155);
-        }
-
-        SECTION("cross product")
-        {
-            REQUIRE(crossProduct(a, b) == Matrix<int, 3, 1>{15, -30, 15});
-        }
-
-        SECTION("copy")
-        {
-            a = b;
-
-            REQUIRE(a == Matrix<int, 3, 1>{1, 3, 5});
-
-            REQUIRE(b == Matrix<int, 3, 1>{1, 3, 5});
-        }
-    }
-
-    SECTION("matrix multiplication")
-    {
-        auto a = Matrix<int, 1, 3>{1, 5, 7};
-        auto b = Matrix<int, 3, 2>{{{11, 13}, {17, 19}, {23, 29}}};
-
-        REQUIRE(a * b == Matrix<int, 1, 2>{257, 311});
-    }
-
-    SECTION("binary scalar-matrix operations")
-    {
-        auto matrix = Matrix<int, 2, 2>{{{10, 15}, {20, 25}}};
-
-        SECTION("multiplication")
-        {
-            REQUIRE(2 * matrix == Matrix<int, 2, 2>{{{20, 30}, {40, 50}}});
-        }
-
-        SECTION("division")
-        {
-            REQUIRE(matrix / 5 == Matrix<int, 2, 2>{{{2, 3}, {4, 5}}});
-        }
-    }
-
-    SECTION("magnitude")
-    {
-        auto vector = Matrix<double, 3, 1>{3.0, 4.0, 5.0};
-
-        REQUIRE(magnitude(vector) == Approx(7.071067f));
-    }
-
-    SECTION("norm")
-    {
-        constexpr auto precision = 1e-5f;
-        auto vector = Matrix<double, 3, 1>{3.0, 4.0, 5.0};
-        auto expected = Matrix<double, 3, 1>{0.424264, 0.565685, 0.707106};
-
-        REQUIRE(close(normalized(vector), expected, precision));
-    }
-
-    SECTION("distance")
-    {
-        auto a = Matrix<double, 3, 1>{1.0, 2.0, 0.0};
-        auto b = Matrix<double, 3, 1>{2.0, 7.0, 5.0};
-
-        REQUIRE(distance(a, b) == Approx(7.1414));
+        REQUIRE(matrix != otherMatrix);
     }
 }
