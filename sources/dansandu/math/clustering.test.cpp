cd942fba68d29f9e247db89ece6602597211d49f
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2021-07-13 20:03:08 +0300

    Parameterize clustering with random engine

--- sources/dansandu/math/clustering.test.cpp
+++ sources/dansandu/math/clustering.test.cpp
@@ -1,13 +1,14 @@
 #include "catchorg/catch/catch.hpp"
+#include "dansandu/ballotin/test/random.hpp"
 #include "dansandu/math/clustering.hpp"
 #include "dansandu/math/matrix.hpp"
 
+using dansandu::ballotin::test::random::PredictableBitGenerator;
 using dansandu::math::clustering::kMeans;
 using dansandu::math::matrix::close;
 using dansandu::math::matrix::Matrix;
 using dansandu::math::matrix::sliceRow;
 
-// clang-format off
 TEST_CASE("clustering")
 {
     SECTION("k-means")
@@ -17,34 +18,32 @@ TEST_CASE("clustering")
         //  * *  *   |    *
         // ----------|-------*--
         //           |      * *
-        //           |    * *
-        //           |
-        const auto samples = Matrix<float>{{{-3.0f,  6.0f},
-                                            {-7.0f,  5.0f},
-                                            {-8.0f,  4.0f},
-                                            {-7.0f,  3.0f},
-                                            {-4.0f,  2.0f},
-                                            { 5.0f,  1.0f},
-                                            { 8.0f,  0.0f},
-                                            { 7.0f, -1.0f},
-                                            { 9.0f, -2.0f},
-                                            { 5.0f, -4.0f},
-                                            { 6.0f, -4.5f}}};
+        //   *       |    * *
+        //  *        |
+        const auto samples = Matrix<float>{{{5.0f, 1.0f},
+                                            {-8.0f, 4.0f},
+                                            {7.0f, -1.0f},
+                                            {-7.0f, 5.0f},
+                                            {-7.0f, 3.0f},
+                                            {-8.0f, -9.0f},
+                                            {-4.0f, 2.0f},
+                                            {-3.0f, 6.0f},
+                                            {8.0f, 0.0f},
+                                            {6.0f, -4.5f},
+                                            {-6.0f, -6.0f},
+                                            {9.0f, -2.0f},
+                                            {5.0f, -4.0f}}};
 
-        const auto clusters = 2;
+        auto generator = PredictableBitGenerator{};
+        const auto clusters = 3;
         const auto iterations = 10;
-        const auto epsilon = 1.0e-5f;
-        const auto [centroids, labels] = kMeans(samples, clusters, iterations);
+        const auto [centroids, labels] = kMeans(samples, clusters, iterations, generator);
+        const auto expectedLabels = std::vector<int>{{1, 2, 1, 2, 2, 0, 2, 2, 1, 1, 0, 1, 1}};
 
         REQUIRE(centroids.rowCount() == clusters);
 
         REQUIRE(centroids.columnCount() == samples.columnCount());
 
-        REQUIRE(static_cast<int>(labels.size()) == samples.rowCount());
-
-        REQUIRE(std::all_of(labels.cbegin(), labels.cend(), [](auto l) { return l == 0 || l == 1; }));
-
-        REQUIRE(!close(sliceRow(centroids, 0), sliceRow(centroids, 1), epsilon));
+        REQUIRE(expectedLabels == labels);
     }
 }
-// clang-format on
