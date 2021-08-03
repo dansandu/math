99c2a087ce6c7737463df1d5dca04be83bbdf27c
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2021-08-04 00:24:01 +0300

    Add matrix equality tests

---
+++ sources/dansandu/math/matrix.equality.cpp
@@ -0,0 +1,88 @@
+#include "catchorg/catch/catch.hpp"
+#include "dansandu/math/matrix.hpp"
+
+using dansandu::math::matrix::dynamic;
+using dansandu::math::matrix::Matrix;
+
+TEST_CASE("matrix.equality")
+{
+    SECTION("static")
+    {
+        const auto matrix = Matrix<int, 3, 2>{{{11, 13}, {17, 19}, {23, 29}}};
+
+        SECTION("to static match")
+        {
+            const auto other = Matrix<int, 3, 2>{{{11, 13}, {17, 19}, {23, 29}}};
+
+            REQUIRE(matrix == other);
+        }
+
+        SECTION("to dynamic rows match")
+        {
+            const auto other = Matrix<int, dynamic, 2>{{{11, 13}, {17, 19}, {23, 29}}};
+
+            REQUIRE(matrix == other);
+        }
+
+        SECTION("to dynamic columns match")
+        {
+            const auto other = Matrix<int, 3, dynamic>{{{11, 13}, {17, 19}, {23, 29}}};
+
+            REQUIRE(matrix == other);
+        }
+
+        SECTION("to dynamic rows and columns match")
+        {
+            const auto other = Matrix<int>{{{11, 13}, {17, 19}, {23, 29}}};
+
+            REQUIRE(matrix == other);
+        }
+
+        SECTION("to dynamic rows mismatch")
+        {
+            const auto other = Matrix<int, dynamic, 2>{{{11, 13}, {17, 19}}};
+
+            REQUIRE(matrix != other);
+        }
+
+        SECTION("to dynamic columns mismatch")
+        {
+            const auto other = Matrix<int, 3, dynamic>{{11, 13, 17}};
+
+            REQUIRE(matrix != other);
+        }
+
+        SECTION("to dynamic rows and columns mismatch")
+        {
+            const auto other = Matrix<int>{{{11, 13}, {17, 19}}};
+
+            REQUIRE(matrix != other);
+        }
+    }
+
+    SECTION("dynamic")
+    {
+        const auto matrix = Matrix<int>{{{31, 37, 41}, {43, 47, 53}}};
+
+        SECTION("to dynamic rows match")
+        {
+            const auto other = Matrix<int, dynamic, 3>{{{31, 37, 41}, {43, 47, 53}}};
+
+            REQUIRE(matrix == other);
+        }
+
+        SECTION("to dynamic columns match")
+        {
+            const auto other = Matrix<int, 2, dynamic>{{{31, 37, 41}, {43, 47, 53}}};
+
+            REQUIRE(matrix == other);
+        }
+
+        SECTION("to dynamic rows and columns match")
+        {
+            const auto other = Matrix<int>{{{31, 37, 41}, {43, 47, 53}}};
+
+            REQUIRE(matrix == other);
+        }
+    }
+}
--- sources/dansandu/math/matrix.static.slicing.cpp
+++ sources/dansandu/math/matrix.static.slicing.cpp
@@ -1,8 +1,6 @@
 #include "catchorg/catch/catch.hpp"
 #include "dansandu/math/matrix.hpp"
 
-#include <vector>
-
 using dansandu::math::matrix::ConstantMatrixView;
 using dansandu::math::matrix::dynamic;
 using dansandu::math::matrix::Matrix;
@@ -21,14 +19,9 @@ TEST_CASE("matrix.static.slicing")
 
         SECTION("data")
         {
-            REQUIRE(view.rowCount() == 1);
+            const auto expected = Matrix<int, 1, 3>{{6, 7, 8}};
 
-            REQUIRE(view.columnCount() == 3);
-
-            const auto expected = std::vector<int>{{6, 7, 8}};
-            const auto actual = std::vector<int>{view.cbegin(), view.cend()};
-
-            REQUIRE(actual == expected);
+            REQUIRE(view == expected);
         }
 
         SECTION("subscript")
@@ -39,13 +32,13 @@ TEST_CASE("matrix.static.slicing")
 
             view(0, 2) = 300;
 
-            const auto expectedMatrix = std::vector<int>{{1, 2, 3, 4, 5, 100, 200, 300, 9, 10, 11, 12}};
+            const auto expectedMatrix = Matrix<int, 3, 4>{{{1, 2, 3, 4}, {5, 100, 200, 300}, {9, 10, 11, 12}}};
 
-            REQUIRE(std::vector<int>{matrix.cbegin(), matrix.cend()} == expectedMatrix);
+            REQUIRE(matrix == expectedMatrix);
 
-            const auto expectedView = std::vector<int>{{100, 200, 300}};
+            const auto expectedView = Matrix<int, 1, 3>{{100, 200, 300}};
 
-            REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expectedView);
+            REQUIRE(view == expectedView);
         }
 
         SECTION("implace addition")
@@ -54,13 +47,13 @@ TEST_CASE("matrix.static.slicing")
 
             view += other;
 
-            const auto expectedMatrix = std::vector<int>{{1, 2, 3, 4, 5, 106, 207, 308, 9, 10, 11, 12}};
+            const auto expectedMatrix = Matrix<int, 3, 4>{{{1, 2, 3, 4}, {5, 106, 207, 308}, {9, 10, 11, 12}}};
 
-            REQUIRE(std::vector<int>{matrix.cbegin(), matrix.cend()} == expectedMatrix);
+            REQUIRE(matrix == expectedMatrix);
 
-            const auto expectedView = std::vector<int>{{106, 207, 308}};
+            const auto expectedView = Matrix<int, 1, 3>{{106, 207, 308}};
 
-            REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expectedView);
+            REQUIRE(view == expectedView);
         }
 
         SECTION("implace subtraction")
@@ -69,63 +62,56 @@ TEST_CASE("matrix.static.slicing")
 
             view -= other;
 
-            const auto expectedMatrix = std::vector<int>{{1, 2, 3, 4, 5, -94, -193, -292, 9, 10, 11, 12}};
-            const auto expectedView = std::vector<int>{{-94, -193, -292}};
+            const auto expectedMatrix = Matrix<int, 3, 4>{{{1, 2, 3, 4}, {5, -94, -193, -292}, {9, 10, 11, 12}}};
 
-            REQUIRE(std::vector<int>{matrix.cbegin(), matrix.cend()} == expectedMatrix);
+            REQUIRE(matrix == expectedMatrix);
 
-            REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expectedView);
+            const auto expectedView = Matrix<int, 1, 3>{{-94, -193, -292}};
+
+            REQUIRE(view == expectedView);
         }
 
         SECTION("additive inverse")
         {
             const auto other = -view;
 
-            const auto expectedOther = std::vector<int>{{-6, -7, -8}};
+            const auto expectedOther = Matrix<int, 1, 3>{{-6, -7, -8}};
 
-            REQUIRE(std::vector<int>{other.cbegin(), other.cend()} == expectedOther);
+            REQUIRE(other == expectedOther);
 
-            const auto expectedView = std::vector<int>{{6, 7, 8}};
+            const auto expectedView = Matrix<int, 1, 3>{{6, 7, 8}};
 
-            REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expectedView);
+            REQUIRE(view == expectedView);
 
-            const auto expectedMatrix = std::vector<int>{{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}};
+            const auto expectedMatrix = Matrix<int, 3, 4>{{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}};
 
-            REQUIRE(std::vector<int>{matrix.cbegin(), matrix.cend()} == expectedMatrix);
+            REQUIRE(matrix == expectedMatrix);
         }
 
         SECTION("scalar multiplication")
         {
             const auto result = 10 * view;
 
-            REQUIRE(result.rowCount() == 1);
+            const auto expectedResult = Matrix<int, 1, 3>{{60, 70, 80}};
 
-            REQUIRE(result.columnCount() == 3);
+            REQUIRE(result == expectedResult);
 
-            const auto expectedResult = std::vector<int>{{60, 70, 80}};
+            const auto expectedView = Matrix<int, 1, 3>{{6, 7, 8}};
 
-            REQUIRE(std::vector<int>{result.cbegin(), result.cend()} == expectedResult);
-
-            const auto expectedView = std::vector<int>{{6, 7, 8}};
-
-            REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expectedView);
+            REQUIRE(view == expectedView);
         }
 
         SECTION("scalar division")
         {
             const auto result = view / 2;
 
-            REQUIRE(result.rowCount() == 1);
+            const auto expectedResult = Matrix<int, 1, 3>{{3, 3, 4}};
 
-            REQUIRE(result.columnCount() == 3);
+            REQUIRE(result == expectedResult);
 
-            const auto expectedResult = std::vector<int>{{3, 3, 4}};
+            const auto expectedView = Matrix<int, 1, 3>{{6, 7, 8}};
 
-            REQUIRE(std::vector<int>{result.cbegin(), result.cend()} == expectedResult);
-
-            const auto expectedView = std::vector<int>{{6, 7, 8}};
-
-            REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expectedView);
+            REQUIRE(view == expectedView);
         }
 
         SECTION("addition")
@@ -134,17 +120,13 @@ TEST_CASE("matrix.static.slicing")
 
             const auto result = view + other;
 
-            REQUIRE(result.rowCount() == 1);
+            const auto expectedResult = Matrix<int, 1, 3>{{16, 107, 1008}};
 
-            REQUIRE(result.columnCount() == 3);
+            REQUIRE(result == expectedResult);
 
-            const auto expectedResult = std::vector<int>{{16, 107, 1008}};
+            const auto expectedView = Matrix<int, 1, 3>{{6, 7, 8}};
 
-            REQUIRE(std::vector<int>{result.cbegin(), result.cend()} == expectedResult);
-
-            const auto expectedView = std::vector<int>{{6, 7, 8}};
-
-            REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expectedView);
+            REQUIRE(view == expectedView);
         }
 
         SECTION("subtraction")
@@ -153,17 +135,13 @@ TEST_CASE("matrix.static.slicing")
 
             const auto result = view - other;
 
-            REQUIRE(result.rowCount() == 1);
+            const auto expectedResult = Matrix<int, 1, 3>{{-4, -4, -4}};
 
-            REQUIRE(result.columnCount() == 3);
+            REQUIRE(result == expectedResult);
 
-            const auto expectedResult = std::vector<int>{{-4, -4, -4}};
+            const auto expectedView = Matrix<int, 1, 3>{{6, 7, 8}};
 
-            REQUIRE(std::vector<int>{result.cbegin(), result.cend()} == expectedResult);
-
-            const auto expectedView = std::vector<int>{{6, 7, 8}};
-
-            REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expectedView);
+            REQUIRE(view == expectedView);
         }
 
         SECTION("multiplication")
@@ -172,53 +150,41 @@ TEST_CASE("matrix.static.slicing")
 
             const auto result = view * other;
 
-            REQUIRE(result.rowCount() == 1);
+            const auto expectedResult = Matrix<int, 1, 2>{{440, 4400}};
 
-            REQUIRE(result.columnCount() == 2);
+            REQUIRE(result == expectedResult);
 
-            const auto expectedResult = std::vector<int>{{440, 4400}};
+            const auto expectedView = Matrix<int, 1, 3>{{6, 7, 8}};
 
-            REQUIRE(std::vector<int>{result.cbegin(), result.cend()} == expectedResult);
-
-            const auto expectedView = std::vector<int>{{6, 7, 8}};
-
-            REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expectedView);
+            REQUIRE(view == expectedView);
         }
     }
 
     SECTION("view to view")
     {
-        const auto view = Slicer<0, 1, 1, 2>::slice(Slicer<1, 1, 2, 3>::slice(matrix));
+        const auto view = Slicer<0, 2, 2, 1>::slice(Slicer<1, 1, 2, 3>::slice(matrix));
 
-        REQUIRE(view.rowCount() == 1);
+        const auto expectedView = Matrix<int, 2, 1>{{8, 12}};
 
-        REQUIRE(view.columnCount() == 2);
-
-        const auto expected = std::vector<int>{{7, 8}};
-
-        REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expected);
+        REQUIRE(view == expectedView);
 
         view(0) = 0;
 
-        const auto expectedMatrix = std::vector<int>{{1, 2, 3, 4, 5, 6, 0, 8, 9, 10, 11, 12}};
+        const auto expectedMatrix = Matrix<int, 3, 4>{{{1, 2, 3, 4}, {5, 6, 7, 0}, {9, 10, 11, 12}}};
 
-        REQUIRE(std::vector<int>{matrix.cbegin(), matrix.cend()} == expectedMatrix);
+        REQUIRE(matrix == expectedMatrix);
 
-        const auto expectedView = std::vector<int>{{0, 8}};
+        const auto expectedMutatedView = Matrix<int, 2, 1>{{0, 12}};
 
-        REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expectedView);
+        REQUIRE(view == expectedMutatedView);
     }
 
     SECTION("container to constant view")
     {
         const auto view = Slicer<1, 2, 2, 1>::slice(constantMatrix);
 
-        REQUIRE(view.rowCount() == 2);
+        const auto expected = Matrix<int, 2, 1>{{7, 11}};
 
-        REQUIRE(view.columnCount() == 1);
-
-        const auto expected = std::vector<int>{{7, 11}};
-
-        REQUIRE(std::vector<int>{view.cbegin(), view.cend()} == expected);
+        REQUIRE(view == expected);
     }
 }
