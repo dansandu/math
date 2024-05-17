b2f13435f973881f1880014c2e4c5aac143c8754
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2024-05-17 20:26:48 +0300

    Update test main

--- sources/dansandu/math/internal/matrix/data_storage_heap.hpp
+++ sources/dansandu/math/internal/matrix/data_storage_heap.hpp
@@ -131,7 +131,7 @@ public:
         if (referenceCount_ != 0)
         {
             THROW(std::logic_error, "there are ", referenceCount_,
-                  " matrix views still pointing to this matrix container\n");
+                  " matrix views still pointing to this matrix container");
         }
         DimensionalityStorage<T, M, N>::operator=(other);
         data_ = other.data_;
