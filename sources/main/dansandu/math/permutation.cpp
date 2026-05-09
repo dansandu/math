7842be5b7c9413c2be9ab939a1fbaafa199c156f
Daniel Sandu <49592096+dansandu@users.noreply.github.com>
2026-05-09 21:41:36 +0300

    Remove std::ranges::to

--- sources/main/dansandu/math/permutation.cpp
+++ sources/main/dansandu/math/permutation.cpp
@@ -1,22 +1,28 @@
 #include "dansandu/math/permutation.hpp"
 #include "dansandu/journey/exception.hpp"
 
-#include <ranges>
 #include <vector>
 
 namespace dansandu::math::permutation
 {
 
-std::vector<int> getIdentityPermutation(const int n)
+std::vector<int> getIdentityPermutation(const int size)
 {
-    if (n >= 0)
+    if (size < 0)
     {
-        return std::views::iota(0, n) | std::ranges::to<std::vector>();
+        THROW(std::logic_error, "Permutation cannot have negative size ", size);
     }
-    else
+
+    auto result = std::vector<int>{};
+
+    result.reserve(size);
+
+    for (auto index = 0; index < size; ++index)
     {
-        THROW(std::logic_error, "permutation cannot be negative value ", n);
+        result.push_back(index);
     }
+
+    return result;
 }
 
 std::vector<int> getInvertedPermutation(const std::vector<int>& permutation)
